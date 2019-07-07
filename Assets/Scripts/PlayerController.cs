fd2bc0b856faa1a448669d2318e08ff5877f5a26
justinjfl <jsnell@knights.ucf.edu>
2019-07-07 01:20:12 -0400

    Tier 2 of challenge 1 completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,21 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Text countText;
     public Text winText;
+    public Text pickupText;
 
     private Rigidbody rb;
-    private int count;
+    private int scoreCount;
+    private int pickupCount;
+    private int yellowCount;
+    public Transform playerLocation;
 
     void Start ()
     {
+        playerLocation = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
-        count = 0;
+        scoreCount = 0;
+        pickupCount = 0;
+        yellowCount = 0;
         SetCountText ();
         winText.text = "";
     }
@@ -43,18 +50,67 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag ("Pick Up"))
         {
             other.gameObject.SetActive (false);
-            count = count + 1;
+            scoreCount = scoreCount + 1;
+            pickupCount = pickupCount + 1;
+            yellowCount = yellowCount + 1;
+            SetCountText ();
+        }
+        else if (other.gameObject.CompareTag ("Enemy"))
+        {
+            other.gameObject.SetActive (false);
+            scoreCount = scoreCount -1 ;
+            pickupCount = pickupCount + 1;
             SetCountText ();
         }
     }
     
     void SetCountText ()
     {
-        countText.text = "Score: " + count.ToString ();
-        if (count >= 12)
+        countText.text = "Score: " + scoreCount.ToString ();
+        pickupText.text = "Pickups Acquired: " + pickupCount.ToString();
+        
+        if (yellowCount == 12)
+        {
+            StartCoroutine(moveDelay(3f));
+            winText.text = "Level 1 Complete.";
+            Invoke("disableText", 3f);
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            Invoke("restorePlayer", 3.5f);
+        }
+        else if (yellowCount ==20 && pickupCount >20)
         {
             countText.text = "";
-            winText.text = "You Lost! (jk)";
+            pickupText.text = "";
+            winText.text = "You Lost!";
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else if (yellowCount == 20 && pickupCount == 20)
+        {
+            countText.text = "";
+            pickupText.text = "";
+            winText.text = "You Won!";
+            rb.constraints = RigidbodyConstraints.FreezeAll;
         }
     }
+    void disableText ()
+    {
+        winText.text = "";
+    }
+
+    void movePlayer()
+    {
+        playerLocation.position = new Vector3(120.0f,2.5f,0);
+    }
+
+    void restorePlayer()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+    }
+
+     IEnumerator moveDelay(float time)
+    {
+         yield return new WaitForSeconds(time);
+         movePlayer();    
+    }
+
 }
